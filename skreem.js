3cce6ff006570be591f48152f579d9dafded7d0f
Guy Hartstein <guyh2020@gmail.com>
2019-11-09 14:33:18 -0800

    Update skreem.js

--- skreem.js
+++ skreem.js
@@ -36,7 +36,7 @@ window.onload = function() {
                 },
                 "optional": []
             },
-        }, gotStream, didntGetStream;
+        }, gotStream, didntGetStream);
     } catch (e) {
         alert('getUserMedia threw exception :' + e);
     }
